4117bbced095c6094a42c3947fd16aabd95777e1
pierre adorni <pierreadorni@free.fr>
2023-06-03 11:50:19 +0200

    grid search on BM

--- noml/stereo_match.py
+++ noml/stereo_match.py
@@ -1,10 +1,11 @@
 import click
+import matplotlib.pyplot as plt
 import numpy as np
 from skimage import io
 from skimage.color import rgb2gray
 
 from block_matching import disp, disp_inv
-from utils import mode_filter_occl, merge_disps
+from utils import mode_filter_occl, merge_disps, mode_filter
 
 
 @click.command()
@@ -20,16 +21,39 @@ def main(left, right, maxdisp=64, block_size=5):
     disp_map = disp(left_img, right_img, int(maxdisp), int(block_size))
 
     # compute the inverse disparity map
-    disp_map_inv = disp_inv(right_img, left_img, int(maxdisp), int(block_size))
+    disp_map_inv = disp_inv(left_img, right_img, int(maxdisp), int(block_size))
 
     # merge the two disparity maps
     disp_map_merged = merge_disps(disp_map, disp_map_inv)
 
-    # apply a mode filter to the merged disparity map
-    disp_map_merged_filtered = mode_filter_occl(disp_map_merged, int(block_size))
+    # apply a mode filter but without the occluded pixels
+    disp_map_merged_filtered_no_occl_11 = mode_filter_occl(disp_map_merged, 11)
+    disp_map_merged_filtered_no_occl_9 = mode_filter_occl(disp_map_merged, 9)
+    disp_map_merged_filtered_no_occl_7 = mode_filter_occl(disp_map_merged, 7)
+    disp_map_merged_filtered_no_occl_5 = mode_filter_occl(disp_map_merged, 5)
+    disp_map_merged_filtered_no_occl_3 = mode_filter_occl(disp_map_merged, 3)
 
-    # save the disparity map
-    io.imsave('disp_map.png', disp_map_merged_filtered)
+    # apply a mode filter
+    disp_map_merged_filtered_11 = mode_filter(disp_map_merged, 11)
+    disp_map_merged_filtered_9 = mode_filter(disp_map_merged, 9)
+    disp_map_merged_filtered_7 = mode_filter(disp_map_merged, 7)
+    disp_map_merged_filtered_5 = mode_filter(disp_map_merged, 5)
+    disp_map_merged_filtered_3 = mode_filter(disp_map_merged, 3)
+
+    # save the disparity maps
+    io.imsave('tests/disp_map_no_occl_11.png', disp_map_merged_filtered_no_occl_11.astype(np.uint8) * 4)
+    io.imsave('tests/disp_map_no_occl_9.png', disp_map_merged_filtered_no_occl_9.astype(np.uint8) * 4)
+    io.imsave('tests/disp_map_no_occl_7.png', disp_map_merged_filtered_no_occl_7.astype(np.uint8) * 4)
+    io.imsave('tests/disp_map_no_occl_5.png', disp_map_merged_filtered_no_occl_5.astype(np.uint8) * 4)
+    io.imsave('tests/disp_map_no_occl_3.png', disp_map_merged_filtered_no_occl_3.astype(np.uint8) * 4)
+
+    io.imsave('tests/disp_map_11.png', disp_map_merged_filtered_11.astype(np.uint8) * 4)
+    io.imsave('tests/disp_map_9.png', disp_map_merged_filtered_9.astype(np.uint8) * 4)
+    io.imsave('tests/disp_map_7.png', disp_map_merged_filtered_7.astype(np.uint8) * 4)
+    io.imsave('tests/disp_map_5.png', disp_map_merged_filtered_5.astype(np.uint8) * 4)
+    io.imsave('tests/disp_map_3.png', disp_map_merged_filtered_3.astype(np.uint8) * 4)
+
+    io.imsave('tests/disp_map.png', disp_map_merged.astype(np.uint8) * 4)
 
 
 if __name__ == '__main__':
--- noml/utils.py
+++ noml/utils.py
@@ -2,20 +2,21 @@ import numpy as np
 
 
 def merge_disps(left: np.ndarray, right: np.ndarray):
-    # for each pixel on the left image, go to the right image and back to the left image and check if we are still on the same pixel (approximately)
-    # if not, we are on an occluded pixel
+    # for each pixel on the left image, go to the right image and back to the left image and check if we are still on
+    # the same pixel (approximately) if not, we are on an occluded pixel
     result = left.copy()
     for j in range(left.shape[0]):
         for i in range(left.shape[1]):
             # get the disparity of the pixel
-            disp = left[j, i]
+            disp = int(left[j, i])
             # get the pixel on the right image
             try:
                 pixel_right = right[j, i - disp]
-                # if the pixel on the right image is not approx. the same as the pixel on the left image, we are on an occluded pixel
+                # if the pixel on the right image is not approx. the same as the pixel on the left image, we are on
+                # an occluded pixel
                 if disp < pixel_right - 10 or disp > pixel_right + 10:
                     result[j, i] = 0
-            except IndexError:
+            except IndexError as e:
                 result[j, i] = 0
 
     return result
@@ -23,7 +24,7 @@ def merge_disps(left: np.ndarray, right: np.ndarray):
 
 def mode_filter_occl(disp_map: np.ndarray, filter_size=5):
     """
-    Apply a mode filter to the disparity map
+    Apply a mode filter to the disparity map, except for the occluded pixels
     :param disp_map: disparity map
     :param filter_size: size of the filter, defaults to 5
     :return: filtered disparity map
@@ -40,6 +41,25 @@ def mode_filter_occl(disp_map: np.ndarray, filter_size=5):
                     mode = np.bincount(values.flatten().astype(int))[1:].argmax() + 1
                 except ValueError:
                     mode = 0
+            # assign the mode to the center of the filter
+            disp_map_filtered[j + filter_size // 2, i + filter_size // 2] = mode
+    return disp_map_filtered
+
+
+def mode_filter(disp_map: np.ndarray, filter_size=5):
+    """
+    Apply a mode filter to the disparity map
+    :param disp_map: disparity map
+    :param filter_size: size of the filter, defaults to 5
+    :return: filtered disparity map
+    """
+    disp_map_filtered = np.zeros_like(disp_map)
+    for j in range(disp_map.shape[0] - filter_size):
+        for i in range(disp_map.shape[1] - filter_size):
+            # get the values in the filter
+            values = disp_map[j:j + filter_size, i:i + filter_size]
+            # compute the mode
+            mode = np.bincount(values.flatten().astype(int)).argmax()
             # assign the mode to the center of the filter
             disp_map_filtered[j + filter_size // 2, i + filter_size // 2] = mode
     return disp_map_filtered
