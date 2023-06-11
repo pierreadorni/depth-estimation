9d09327c597c10db7a6453a40bebf5c39ee5e62a
zhixuanFrank <122116172+zhixuanFrank@users.noreply.github.com>
2023-06-11 22:49:51 +0200

    Add files via upload

---
+++ PD.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 10 16:09:24 2023
+
+@author: fengzhixuan
+"""
+
+import numpy as np
+import argparse
+from skimage import io
+import cv2
+import matplotlib.pyplot as plt
+
+def calculate_disparity_map(left_image, right_image, window_size, max_disparity):
+    height, width = left_image.shape
+    disparity_map = np.zeros((height, width), dtype=np.float32)
+    cost_matrix = np.zeros((height, width, max_disparity), dtype=np.float32)
+    
+    for y in range(height):
+        for x in range(width):
+            for d in range(max_disparity):
+                left_patch = get_patch(left_image, x, y, window_size)
+                right_patch = get_patch(right_image, x - d, y, window_size)
+                cost = calculate_cost(left_patch, right_patch)
+                cost_matrix[y, x, d] = cost
+    
+    for y in range(height):
+        for x in range(width):
+            disparity_map[y, x] = np.argmin(cost_matrix[y, x])
+    
+    return disparity_map
+
+def get_patch(image, x, y, window_size):
+    height, width = image.shape
+    half_size = window_size // 2
+    patch = np.zeros((window_size, window_size), dtype=image.dtype)
+    
+    for i in range(window_size):
+        for j in range(window_size):
+            patch[i, j] = image[max(0, min(y - half_size + i, height - 1)), max(0, min(x - half_size + j, width - 1))]
+    
+    return patch
+
+def calculate_cost(left_patch, right_patch):
+    return np.sum(np.abs(left_patch - right_patch))
+
+# 读取左右视图图像
+left_image = io.imread("im2.png", 0)
+right_image = io.imread("im6.png", 0)
+
+# 转换图像数据类型为灰度图像
+left_gray = cv2.cvtColor(left_image, cv2.COLOR_BGR2GRAY)
+right_gray = cv2.cvtColor(right_image, cv2.COLOR_BGR2GRAY)
+window_size = 1
+max_disparity = 100
+
+disparity_map = calculate_disparity_map(left_gray, right_gray, window_size, max_disparity)
+
+plt.imshow(disparity_map, cmap='gray')
+plt.colorbar()
+plt.show()
---
+++ SGBM.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 10 16:14:54 2023
+
+@author: fengzhixuan
+"""
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+# 读取左右视图图像
+left_image = cv2.imread("im2.png", cv2.IMREAD_GRAYSCALE)
+right_image = cv2.imread("im6.png", cv2.IMREAD_GRAYSCALE)
+
+# 特征匹配
+orb = cv2.ORB_create()
+kp1, des1 = orb.detectAndCompute(left_image, None)
+kp2, des2 = orb.detectAndCompute(right_image, None)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+matches = bf.match(des1, des2)
+matches = sorted(matches, key=lambda x: x.distance)
+good_matches = matches[:100]  # 选择前100个最佳匹配点
+
+# 提取关键点坐标
+points1 = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+points2 = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+# 使用RANSAC估计基础矩阵
+F, mask = cv2.findFundamentalMat(points1, points2, cv2.RANSAC, 3.0, 0.99)
+
+# 应用基础矩阵剔除错误匹配点
+points1 = points1[mask.ravel() == 1]
+points2 = points2[mask.ravel() == 1]
+
+# 定义SGBM参数
+window_size = 8
+min_disp = 0
+num_disp = 16 * 5
+
+# 预处理图像
+left_image = cv2.GaussianBlur(left_image, (5, 5), 0)
+right_image = cv2.GaussianBlur(right_image, (5, 5), 0)
+
+# 代价计算
+stereo = np.zeros((left_image.shape[0], left_image.shape[1], num_disp), dtype=np.float32)
+for d in range(num_disp):
+    stereo[:, :, d] = cv2.absdiff(left_image, np.roll(right_image, d))
+
+# 代价聚合
+disparity = np.zeros_like(left_image, dtype=np.float32)
+for y in range(left_image.shape[0]):
+    for x in range(left_image.shape[1]):
+        min_cost = float('inf')
+        best_disparity = 0
+        for d in range(num_disp):
+            cost = np.sum(stereo[max(0, y - window_size // 2):min(y + window_size // 2 + 1, left_image.shape[0]),
+                                max(0, x - window_size // 2):min(x + window_size // 2 + 1, left_image.shape[1]), d])
+            if cost < min_cost:
+                min_cost = cost
+                best_disparity = d
+        disparity[y, x] = best_disparity
+        
+    
+# avec ZNSSD        
+    
+# 代价计算
+# stereo = np.zeros((left_image.shape[0], left_image.shape[1], num_disp), dtype=np.float32)
+# for d in range(num_disp):
+#     diff = (left_image - np.roll(right_image, d)) ** 2
+#     mean_diff = np.mean(diff)
+#     std_diff = np.std(diff)
+#     normalized_diff = (diff - mean_diff) / std_diff
+#     stereo[:, :, d] = normalized_diff
+
+# 代价聚合
+# disparity = np.zeros_like(left_image, dtype=np.float32)
+# for y in range(left_image.shape[0]):
+#     for x in range(left_image.shape[1]):
+#         min_cost = float('inf')
+#         best_disparity = 0
+#         for d in range(num_disp):
+#             cost = np.sum(stereo[max(0, y - window_size // 2):min(y + window_size // 2 + 1, left_image.shape[0]),
+#                                 max(0, x - window_size // 2):min(x + window_size // 2 + 1, left_image.shape[1]), d])
+#             if cost < min_cost:
+#                 min_cost = cost
+#                 best_disparity = d
+#         disparity[y, x] = best_disparity
+
+
+# 调整视差范围和缩放因子
+disparity = (disparity - min_disp) / num_disp
+
+# 显示视差图
+plt.imshow(disparity, cmap='gray')
+plt.colorbar()
+plt.show()
+
+
+
+
+
+
+
+
+
+
